2fcc574684b40f80eae6ba90ac16ce15b5bbcdca
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-20 23:10:44 +0200

    genre

--- js/module.js
+++ js/module.js
@@ -7,17 +7,23 @@ const addNewGame = (newGame) => {
     const titleGame = document.getElementById("title").value;
     const fotoGame = document.getElementById("foto").value;
     const descriptionGame = document.getElementById("description").value;
-    const shutterGame = document.getElementById("shutter").value;
-    const adventureGame = document.getElementById("adventure").value;
-    const fightGame = document.getElementById("fight").value;
+    const shutterGame = document.getElementById("shutter");
+    const adventureGame = document.getElementById("adventure");
+    const fightGame = document.getElementById("fight");
     const ratingGame = document.getElementById("rating").value;
-    const genreArray = []
+    const genreArray = [shutterGame, adventureGame, fightGame];
+    let genreNewArray = [];
 
+    for (game of genreArray) {
+        if (game.checked === true) {
+            genreNewArray.push(game.value)
+        }
+    }
     newGame = {
         name: titleGame,
         foto: fotoGame,
         description: descriptionGame,
-        genre: [shutterGame,adventureGame,fightGame],
+        genre: genreNewArray,
         rating: ratingGame
     }
     gameList.push(newGame)

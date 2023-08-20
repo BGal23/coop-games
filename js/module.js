ef4b49977294913c99247612dcf12b3fd218d3d0
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-20 22:53:56 +0200

    new

--- js/module.js
+++ js/module.js
@@ -1,4 +1,28 @@
 "use strick";
 
-const titleGame = document.getElementById("title").value;
-const descriptionGame = document.getElementById("description").value;
+const buttonNewGame = document.getElementById("button-game");
+let gameList = [];
+
+const addNewGame = (newGame) => {
+    const titleGame = document.getElementById("title").value;
+    const fotoGame = document.getElementById("foto").value;
+    const descriptionGame = document.getElementById("description").value;
+    const shutterGame = document.getElementById("shutter").value;
+    const adventureGame = document.getElementById("adventure").value;
+    const fightGame = document.getElementById("fight").value;
+    const ratingGame = document.getElementById("rating").value;
+    const genreArray = []
+
+    newGame = {
+        name: titleGame,
+        foto: fotoGame,
+        description: descriptionGame,
+        genre: [shutterGame,adventureGame,fightGame],
+        rating: ratingGame
+    }
+    gameList.push(newGame)
+
+    console.log(gameList)
+}
+
+buttonNewGame.addEventListener("click", addNewGame)
